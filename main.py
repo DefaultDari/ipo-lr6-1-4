6ce6e0a0349e2133879705b5d1e5e26110c09910
Даниил Луценко <110895420+DefaultDari@users.noreply.github.com>
2024-11-17 16:21:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,14 @@
 #- Находит и выводит все строки, которые содержат искомую подстроку, а также их количество из следующего файла.
 #- Сортирует найденные строки в порядке их длины (от самой короткой к самой длинной) и выводит их.
 
-def find_and_sort_lines_with_substring(filename, substring):
-    file =  open(filename, 'r', encoding='utf-8') # Открываем файл с учетом кодировки
-    lines = file.readlines() # Читаем строки в файле
-    substring = substring.lower() # Приводим подстроку к нижнему регистру для нечувствительности к регистру
-    matching_lines = [line.strip() for line in lines if substring in line.lower()]  # Находим строки, содержащие подстроку, независимо от регистра
-    print(f"\nКоличество строк, содержащих подстроку '{substring}': {len(matching_lines)}") #  Выводим количество найденных строк
-    matching_lines.sort(key=len) # Сортируем строки по длине
-    # Выводим отсортированные строки
-    print("\nСтроки, содержащие подстроку, отсортированные по длине:")
-    for line in matching_lines:
-        print(line)
-search_string = input("Введите строку для поиска: ") # Запрос строки для поиска
-find_and_sort_lines_with_substring("C:\\Users\\agjjj\\Desktop\\text.txt", search_string) # Поиск и сортировка строк в указанном файле
+search_string = input("Введите строку для поиска: ")  # Запрос строки для поиска
+with open("text.txt", 'r', encoding='utf-8') as file: # Открываем файл с учетом кодировки
+    lines = file.readlines()  # Читаем строки в файле
+search_string = search_string.lower() # Приводим подстроку к нижнему регистру для нечувствительности к регистру
+matching_lines = [line.strip() for line in lines if search_string in line.lower()] # Находим строки, содержащие подстроку, независимо от регистра
+print(f"\nКоличество строк, содержащих подстроку '{search_string}': {len(matching_lines)}") # Выводим количество найденных строк
+matching_lines.sort(key=len) # Сортируем строки по длине
+# Выводим отсортированные строки
+print("\nСтроки, содержащие подстроку, отсортированные по длине:")
+for line in matching_lines:
+    print(line)
